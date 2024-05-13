96b08892de2e55bd936f3d08d9e1eec31b44b647
David Clain <david974.clain@gmail.com>
2024-05-13 12:56:59 +0400

    Initial Commit

---
+++ OpenQuizz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenQuizz
+//
+//  Created by David Clain on 13/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
