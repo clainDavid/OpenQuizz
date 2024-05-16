764c588c9f6b2d0db0ba4789da0317ccc032bc79
David Clain <david974.clain@gmail.com>
2024-05-16 19:53:43 +0400

    structure the questions

---
+++ OpenQuizz/Model/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  OpenQuizz
+//
+//  Created by David Clain on 16/05/2024.
+//
+
+import Foundation
+
+struct Question {
+    var title: String
+    var isCorrect: Bool
+}
